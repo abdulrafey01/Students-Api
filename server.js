e60871e851397bec7e5a844b20996bcb06c72c72
= <=>
2023-10-07 14:13:33 -0700

    Test 1

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors())
 connectToMongoose()
 
 app.get("/", (req, res) => {
-    res.send("Hello World. Student Api is Running")
+    res.send("Hello World. Student Api Here")
 })
 
 app.use('/student', require('./routes/studentRoutes'))
